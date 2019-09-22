144f431363f541df3dc2ab3283269a4feeefbaee
igorgcosta <igorgabriel@outlook.com>
2019-09-22 14:30:54 -0300

    adição de requisição http e print no stdout

--- go-app/main.go
+++ go-app/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"crypto/rand"
 	"encoding/base64"
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -14,27 +15,29 @@ const (
 	filepath = "./arquivo-go.txt"
 )
 
-func main() {
-	http.HandleFunc("/", handle)
-	fmt.Fprintf(os.Stdout, "listening on 8081")
-	log.Fatal(http.ListenAndServe(":8081", nil))
-}
-
-func handle(w http.ResponseWriter, req *http.Request) {
+func handler(w http.ResponseWriter, req *http.Request) {
+	// Lê os dados de um arquivo específico,
+	// concatena aos dados uma string randômica
+	// e salva o arquivo com os novos dados
 	data, err := ioutil.ReadFile(filepath)
 	checkError(err, w)
-
-	rs, err := generateRandomString()
+	rs, err := generateRandomString(256)
 	checkError(err, w)
-
-	content := append(data, rs+"\n"...)
-
+	content := append(data, fmt.Sprintf("%s\n", rs)...)
 	err = ioutil.WriteFile(filepath, content, 0644)
 	checkError(err, w)
 
+	// Usa os dados randômicos gerados anteriormente
+	// para criar um novo arquivo numa pasta específica
+	// O nome do arquivo é gerado randômicamente
 	// err = generateRandomFile(content)
 	// checkError(err, w)
 
+	// Chama uma api http com dados mock,
+	// escreve no stdout as informações retornadas
+	// logMockApiresponse()
+
+	// Retorna os dados no response body
 	fmt.Fprintf(w, string(content))
 }
 
@@ -44,10 +47,8 @@ func checkError(err error, w http.ResponseWriter) {
 	}
 }
 
-func generateRandomString() (string, error) {
-	size := 32
+func generateRandomString(size int) (string, error) {
 	rb := make([]byte, size)
-
 	_, err := rand.Read(rb)
 	if err != nil {
 		return "", err
@@ -59,12 +60,50 @@ func generateRandomString() (string, error) {
 }
 
 func generateRandomFile(content []byte) error {
-	rs, err := generateRandomString()
+	rs, err := generateRandomString(10)
 	if err != nil {
 		return err
 	}
 
-	ioutil.WriteFile("./contents/"+rs+".txt", content, 0644)
+	ioutil.WriteFile(fmt.Sprintf("./contents/%s.txt", rs), content, 0644)
 
 	return nil
+}
+
+type user struct {
+	ID   string `json:"id"`
+	Name string `json:"name"`
+}
+
+type users []user
+
+func logMockApiresponse() error {
+	url := "http://5d879522cd71160014aaeac7.mockapi.io/api/v1/users"
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return err
+	}
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	users := new(users)
+	json.NewDecoder(resp.Body).Decode(users)
+
+	for _, u := range *users {
+		fmt.Fprintf(os.Stdout, "\nid: %s \nnome: %s\n", u.ID, u.Name)
+	}
+
+	return nil
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	fmt.Fprintf(os.Stdout, "listening on 8081")
+	log.Fatal(http.ListenAndServe(":8081", nil))
 }
--- node-app/app.js
+++ node-app/app.js
@@ -3,6 +3,7 @@ const { readFile, writeFile } = require('fs')
 const path = require('path')
 const crypto = require('crypto')
 const { promisify } = require('util')
+const axios = require('axios')
 
 const app = express()
 const readFileProm = promisify(readFile)
@@ -10,27 +11,50 @@ const writeFileProm = promisify(writeFile)
 
 const filepath = path.resolve(`${__dirname}/arquivo-node.txt`)
 
-app.get('/', async (_req, res) => {
+const handler = async (_req, res) => {
   try {
+    // Lê os dados de um arquivo específico, 
+    // concatena aos dados uma string randômica
+    // e salva o arquivo com os novos dados
     const data = await readFileProm(filepath, 'utf-8')
-
-    const content = `${data}\n${generateRandomString()}`
-
+    const content = `${data}\n${generateRandomString(256)}`
     await writeFileProm(filepath, content)
     
+    // Usa os dados randômicos gerados anteriormente
+    // para criar um novo arquivo numa pasta específica
+    // O nome do arquivo é gerado randômicamente
     // await generateRandomFile(content)
 
+    // Chama uma api http com dados mock,
+    // escreve no stdout as informações retornadas
+    // await logMockApiResponse()
+
+    // Retorna os dados no response body
     return res.status(200).send(content)
 
   } catch (err) {
     console.log(err)
     return res.status(404).end()
   }
-})
+}
 
-const generateRandomFile = (content) => writeFileProm(`./contents/${generateRandomString()}.txt`, content)
-const generateRandomString = () => crypto.randomBytes(20).toString('hex')
+const generateRandomString = (bytes) => crypto.randomBytes(bytes).toString('hex')
 
+const generateRandomFile = (content) => writeFileProm(`./contents/${generateRandomString(10)}.txt`, content)
 
+const logMockApiResponse = async () => {
+  const response = await axios({ 
+    method: 'get', 
+    url: 'http://5d879522cd71160014aaeac7.mockapi.io/api/v1/users'
+  })
+
+  response.data
+    .forEach(user => console.log(`\nid: ${user.id}\nNome: ${user.name}\n`))
+
+  return true
+}
+
+app.get('/', handler)
+// Start server
 const port = process.env.APP_PORT || 8080
 app.listen(port, () => console.log(`Server listening in ${port}`))
